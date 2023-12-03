0fc03694644691466329692e4e21bf29fada8464
Richard <kogunyale01@gmail.com>
2023-12-03 14:42:14 +0000

    Formatting commands

--- src/connection_manager/commands.rs
+++ src/connection_manager/commands.rs
@@ -1,25 +1,18 @@
-use std::{
-    borrow::Cow,
-    time::{UNIX_EPOCH, Duration, SystemTime}
-};
-use crate::{
-    resp::serialize::{serialize, InputVariants},
-    database::db::Cache
-};
-use super::utils::{throw_err_if_num_args_wrong, serialize_error};
+use super::utils::throw_err_if_num_args_wrong;
+use crate::resp::serialize::{serialize, InputVariants};
+use std::borrow::Cow;
 
-
-pub fn handle_echo(args: &[String]) -> Cow<'static, str>{
-    match args.len(){
+pub fn handle_echo(args: &[String]) -> Cow<'static, str> {
+    match args.len() {
         1 => serialize(InputVariants::StringVariant(args[0].clone())),
-        _ => throw_err_if_num_args_wrong("echo")
+        _ => throw_err_if_num_args_wrong("echo"),
     }
 }
 
-pub fn handle_ping() -> Cow<'static, str>{
+pub fn handle_ping() -> Cow<'static, str> {
     serialize(InputVariants::StringVariant("Pong".to_string()))
 }
 
-pub fn ignore_command() -> Cow<'static, str>{
+pub fn ignore_command() -> Cow<'static, str> {
     serialize(InputVariants::Null)
 }
